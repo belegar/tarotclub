f7c10b73ac4364bbaa27c048785a723e13124e1e
belegar <anthony.rabine@tarotclub.fr>
2017-01-26 09:48:44 +0700

    Added windows console ctrl+X key detection

--- console/main.cpp
+++ console/main.cpp
@@ -30,6 +30,37 @@
 #include "GetOptions.h"
 #include "ConsoleClient.h"
 
+#include <windows.h>
+
+// You can fill-in your own behavior, this is just an example handler that
+//   covers most of the most common events.
+BOOL
+ControlHandler (DWORD dwControlEvent)
+{
+  switch (dwControlEvent)
+  {
+    // User wants to shutdown
+    case CTRL_SHUTDOWN_EVENT:
+      return FALSE;
+
+    // User wants to logoff
+    case CTRL_LOGOFF_EVENT:
+      return FALSE;
+
+    // Ctrl + C
+    case CTRL_C_EVENT:
+      return TRUE;
+
+    // User wants to exit the "normal" way
+    case CTRL_CLOSE_EVENT:
+      return TRUE;
+
+    // Everything else, just ignore it...
+    default:
+      return FALSE;
+  }
+}
+
 /*****************************************************************************/
 /**
  * @brief Entry point of the dedicated game server
@@ -55,6 +86,12 @@ int main(int /*argc*/, char ** /*argv[]*/)
     options.localHostOnly = true;
 
     server.Start(options); // Blocking call. On exit, quit the executable
+
+
+    // Set the control handler so the app will be notified upon any special
+    //   termination event.
+    SetConsoleCtrlHandler ((PHANDLER_ROUTINE) ControlHandler, TRUE);
+
 
     client.Run(options.game_tcp_port);
 
